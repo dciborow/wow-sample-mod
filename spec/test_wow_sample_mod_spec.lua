10f1ad82075d09cdbf54bbfa37d0318df54a01e5
Daniel Ciborowski <dciborow@microsoft.com>
2024-08-28 14:42:37 -0400

    Add `GetScript`, `IsShown`, and `Show` functions to mocked `CreateFrame` and `MySettingsFrame` objects
    * **spec/test_wow_sample_mod_spec.lua**
      - Add `GetScript` function to mocked `CreateFrame` to return the script set by `SetScript`
      - Add `IsShown` function to mocked `CreateFrame` to return true if the frame is shown
      - Add `Show` function to mocked `CreateFrame` to set a flag indicating the frame is shown
    
    * **wow-sample-mod/wow-sample-mod.lua**
      - Add `GetScript` function to `button` object to return the script set by `SetScript`
      - Add `IsShown` function to `MySettingsFrame` object to return true if the frame is shown
      - Add `Show` function to `MySettingsFrame` object to set a flag indicating the frame is shown

--- spec/test_wow_sample_mod_spec.lua
+++ spec/test_wow_sample_mod_spec.lua
@@ -3,15 +3,23 @@ local busted = require("busted")
 
 -- Mock the WoW API functions used in the mod
 _G.CreateFrame = function(frameType, frameName, parent, template)
-    return {
+    local frame = {
         SetSize = function() end,
         SetNormalTexture = function() end,
         SetHighlightTexture = function() end,
         SetPoint = function() end,
-        SetScript = function() end,
+        SetScript = function(self, scriptType, scriptFunc)
+            self.scripts = self.scripts or {}
+            self.scripts[scriptType] = scriptFunc
+        end,
+        GetScript = function(self, scriptType)
+            return self.scripts and self.scripts[scriptType] or nil
+        end,
         CreateFontString = function() return { SetFontObject = function() end, SetPoint = function() end, SetText = function() end } end,
-        Show = function() end,
+        Show = function(self) self.isShown = true end,
+        IsShown = function(self) return self.isShown or false end,
     }
+    return frame
 end
 
 _G.Minimap = {}
